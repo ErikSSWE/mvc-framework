a07a6fec0c967b12af6aa2d04c7242a99d7548cb
Erik Svensson <erik@familjensvensson.se>
2021-05-10 10:11:55 +0200

    för kmom05 klart

--- src/Controller/YatzyController.php
+++ src/Controller/YatzyController.php
@@ -7,14 +7,28 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Yatzy\YatzyGame;
+use App\Entity\Highscore;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
 
 class YatzyController extends AbstractController
 {
-    
+    /**
+     * @Route("/yatzy", name="yatzy")
+     */
     public function Yatzy(): Response
     {
-        $game = new YatzyGame();
-
+        $repository = $this->getDoctrine()->getRepository(Highscore::class);
+        $highscores = $repository->topTenScore();
+        $game = new YatzyGame($repository);
+        if ($game->checkHighScore() == true && $game->checker == true) {
+            return $this->redirectToRoute('new', ['score' => $game->getHighscore()]);
+        }
         return $this->render('yatzy.html.twig', $game->getData());
     }
 }
--- src/Yatzy/YatzyGame.php
+++ src/Yatzy/YatzyGame.php
@@ -4,7 +4,13 @@ declare(strict_types=1);
 
 namespace App\Yatzy;
 
+
 use App\Dice\DiceHand;
+use App\Entity\Highscore;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\HighscoreRepository;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 class YatzyGame
 {
@@ -21,27 +27,43 @@ class YatzyGame
         ]
     ];
 
-    public function __construct()
+    var $checker = false;
+
+    /**
+     * @var HighscoreRepository
+     */
+    protected $highscoreRepository;
+
+    /**
+     * OneLevel Constructor
+     *
+     * @param HighscoreRepository $manager
+     */
+    public function __construct(HighscoreRepository $highscoreRepository)
     {
+        $this->session = new Session();
+        //$this->session->set('test', +1);
+        //echo "test:::" . $this->session->get('test');
 
-        $this->hand["throws"] = $_SESSION["rolls"] ?? 0;
-        $this->hand["dices"][1] = $_SESSION["dice1"] ?? 0;
-        $this->hand["dices"][2] = $_SESSION["dice2"] ?? 0;
-        $this->hand["dices"][3] = $_SESSION["dice3"] ?? 0;
-        $this->hand["dices"][4] = $_SESSION["dice4"] ?? 0;
-        $this->hand["dices"][5] = $_SESSION["dice5"] ?? 0;
+        $this->highscoreRepository = $highscoreRepository;
 
-        $_SESSION["message"] = $this->hand["dices"][1];
+        $this->hand["throws"] = $this->session->get("rolls") ?? 0;
+        $this->hand["dices"][1] = $this->session->get('dice1') ?? 0;
+        $this->hand["dices"][2] = $this->session->get('dice2') ?? 0;
+        $this->hand["dices"][3] = $this->session->get('dice3') ?? 0;
+        $this->hand["dices"][4] = $this->session->get('dice4') ?? 0;
+        $this->hand["dices"][5] = $this->session->get('dice5') ?? 0;
 
+        $this->checker = false;
 
         $action = strtolower($_POST["action"] ?? "");
 
         switch ($action) {
             case 'nästa':
-                if ($_SESSION["number"] <= 6) {
+                if ($this->session->get("number") <= 6) {
                     $this->setScore();
-                    $_SESSION["rolls"] = 0;
-                    $_SESSION["number"] += 1;
+                    $this->session->set("rolls", 0);
+                    $this->addValueToSessionVar('number', 1);
                 }
                 $_SESSION["message"] = "Spelet är nu avklarat!";
                 break;
@@ -51,71 +73,83 @@ class YatzyGame
             case 'starta':
                 $this->startaGame();
                 break;
+            case 'avsluta':
+                $this->checker = true;
+                break;
         }
     }
 
     public function setScore()
     {
         foreach ($this->hand["dices"] as $value) {
-            if ($value == $_SESSION["number"]) {
-                $_SESSION["Score" . $_SESSION["number"]] += $value;
+            if ($value == $this->session->get("number")) {
+                $number = $this->session->get("number");
+                $this->addValueToSessionVar("Score" . $number, $value);
             }
         }
-        $_SESSION["dice1"] = 0;
-        $_SESSION["dice2"] = 0;
-        $_SESSION["dice3"] = 0;
-        $_SESSION["dice4"] = 0;
-        $_SESSION["dice5"] = 0;
+        
+        $this->session->set("dice1", 0);
+        $this->session->set("dice2", 0);
+        $this->session->set("dice3", 0);
+        $this->session->set("dice4", 0);
+        $this->session->set("dice5", 0);
     }
 
     public function startaGame()
     {
-        $_SESSION["rolls"] = 0;
-        $_SESSION["dice1"] = 0;
-        $_SESSION["dice2"] = 0;
-        $_SESSION["dice3"] = 0;
-        $_SESSION["dice4"] = 0;
-        $_SESSION["dice5"] = 0;
-        $_SESSION["number"] = 1;
-        $_SESSION["Score1"] = 0;
-        $_SESSION["Score2"] = 0;
-        $_SESSION["Score3"] = 0;
-        $_SESSION["Score4"] = 0;
-        $_SESSION["Score5"] = 0;
-        $_SESSION["Score6"] = 0;
+        $this->session->set("rolls", 0);
+        $this->session->set("dice1", 0);
+        $this->session->set("dice2", 0);
+        $this->session->set("dice3", 0);
+        $this->session->set("dice4", 0);
+        $this->session->set("dice5", 0);
+        $this->session->set("number", 1);
+        $this->session->set("Score1", 0);
+        $this->session->set("Score2", 0);
+        $this->session->set("Score3", 0);
+        $this->session->set("Score4", 0);
+        $this->session->set("Score5", 0);
+        $this->session->set("Score6", 0);
+    }
+
+    public function addValueToSessionVar($name, $value)
+    {
+        $val = $this->session->get($name);
+        $newVal = $val += $value;
+        $newVal = $this->session->set($name, $newVal);
     }
 
 
     public function continueGame()
     {
-        if ($_SESSION["rolls"] < 3) {
+        if ($this->session->get("rolls") < 3) {
             $this->dices();
-            $_SESSION["rolls"] += 1;
+            $this->addValueToSessionVar("rolls", 1);
         }
-        $_SESSION["message"] = $_SESSION["number"];
+        $this->session->set("message", $this->session->get("number"));
     }
 
 
     public function dices()
     {
         if (isset($_POST['dice1']) && $_POST['dice1'] === "1") {
-            $_SESSION["dice1"] = rand(1, 6);
+            $this->session->set("dice1", rand(1, 6));
         }
 
         if (isset($_POST['dice2']) && $_POST['dice2'] === "1") {
-            $_SESSION["dice2"] = rand(1, 6);
+            $this->session->set("dice2", rand(1, 6));
         }
 
         if (isset($_POST['dice3']) && $_POST['dice3'] === "1") {
-            $_SESSION["dice3"] = rand(1, 6);
+            $this->session->set("dice3", rand(1, 6));
         }
 
         if (isset($_POST['dice4']) && $_POST['dice4'] === "1") {
-            $_SESSION["dice4"] = rand(1, 6);
+            $this->session->set("dice4", rand(1, 6));
         }
 
         if (isset($_POST['dice5']) && $_POST['dice5'] === "1") {
-            $_SESSION["dice5"] = rand(1, 6);
+            $this->session->set("dice5", rand(1, 6));
         }
     }
 
@@ -124,20 +158,50 @@ class YatzyGame
         return [
             "title" => "Yatzy",
             "guide" => "För att spela klicka först starta
-                , sen är det bara att klicka i dem du vill slå om",
-            "message" => $_SESSION["message"],
-            "dice1" => $_SESSION["dice1"]  ?? 0,
-            "dice2" => $_SESSION["dice2"] ?? 0,
-            "dice3" => $_SESSION["dice3"] ?? 0,
-            "dice4" => $_SESSION["dice4"] ?? 0,
-            "dice5" => $_SESSION["dice5"] ?? 0,
-            "rolls" => $_SESSION["rolls"] ?? 0,
-            "Score1" => $_SESSION["Score1"] ?? 0,
-            "Score2" => $_SESSION["Score2"] ?? 0,
-            "Score3" => $_SESSION["Score3"] ?? 0,
-            "Score4" => $_SESSION["Score4"] ?? 0,
-            "Score5" => $_SESSION["Score5"] ?? 0,
-            "Score6" => $_SESSION["Score6"] ?? 0,
+                , sen är det bara att klicka i dem du vill slå om
+                när du är klar klickar du bara avsluta",
+            "message" => $this->session->get("message"),
+            "dice1" => $this->session->get("dice1")  ?? 0,
+            "dice2" => $this->session->get("dice2") ?? 0,
+            "dice3" => $this->session->get("dice3") ?? 0,
+            "dice4" => $this->session->get("dice4") ?? 0,
+            "dice5" => $this->session->get("dice5") ?? 0,
+            "rolls" => $this->session->get("rolls") ?? 0,
+            "Score1" => $this->session->get("Score1") ?? 0,
+            "Score2" => $this->session->get("Score2") ?? 0,
+            "Score3" => $this->session->get("Score3") ?? 0,
+            "Score4" => $this->session->get("Score4") ?? 0,
+            "Score5" => $this->session->get("Score5") ?? 0,
+            "Score6" => $this->session->get("Score6") ?? 0,
         ];
+    }
+
+
+    public function getHighscore() 
+    {
+        $totalScore = $this->session->get("Score1") ?? 0;
+        $totalScore += $this->session->get("Score2") ?? 0;
+        $totalScore += $this->session->get("Score3") ?? 0;
+        $totalScore += $this->session->get("Score4") ?? 0;
+        $totalScore += $this->session->get("Score5") ?? 0;
+        $totalScore += $this->session->get("Score6") ?? 0;
+
+        return $totalScore;
+    }
+
+    public function checkHighScore()
+    {
+        if ($this->getHighscore()  > $this->showHighscore())
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public function showHighscore()
+    {
+        //$repository = $this->highscoreRepository->getRepository(Highscore::class);
+        $highscores = $this->highscoreRepository->topTenScore();
+        return $highscores[9]->getScore();
     }
 }
